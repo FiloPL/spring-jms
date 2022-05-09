cf286cafd651f8bde83156e7a80d6c1e6047fc12
FiloPL <tzegarlicki@gmail.com>
2022-05-09 23:32:21 +0200

    Initial commit

---
+++ src/test/java/com/filo/springjms/SpringJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filo.springjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
